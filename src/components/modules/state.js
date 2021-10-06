3c59b7c6cf047a8dca3e864fbfedee7081f8989b
Prince-Thind <princethind799@gmail.com>
2021-10-06 14:53:02 +0530

    create basic state functionality

---
+++ src/components/modules/state.js
@@ -0,0 +1,7 @@
+const state={
+    ships:0,
+}
+function setState(key,value){
+    state[key]=value;
+}
+export {state,setState};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css'
 import './components/styles/start-menu.css'
+import {state,setState} from './components/modules/state';
+
+const shipForm=document.querySelector('#ship-form');
+const menuSection=document.querySelector('#start-menu');
+const shipSelectionSection=document.querySelector('#ship-selection-menu');
+
+shipForm.addEventListener('submit',storeShipInput);
+
+function storeShipInput(e){
+    e.preventDefault();
+    const shipNumber=e.target.ships.value;
+    setState('ships',shipNumber);
+    renderSelectionScreen();
+}
+
+function renderSelectionScreen() {
+    menuSection.classList.toggle('hidden');
+    shipSelectionSection.classList.toggle('hidden');
+}

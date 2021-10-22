7d66a4d28c7abdfc91b38656153a3b171d8f9060
Prince-Thind <princethind799@gmail.com>
2021-10-22 14:38:15 +0530

    fix duplicate enemy move bug

--- src/components/modules/main/playEnemyRound.js
+++ src/components/modules/main/playEnemyRound.js
@@ -1,12 +1,24 @@
 import { battleGround } from '../state/battleground';
 
+const madeMoves=[];
+
 function playEnemyRound() {
-  const random =
-    Math.trunc(Math.random() * 9) + '' + Math.trunc(Math.random() * 9);
+  const random = getRandomMove();
   const gridElement = battleGround.player[random[0]][random[1]];
-  // if (gridElement.empty == 0) {
-    gridElement.hit = 1;
-  // }
+  gridElement.hit = 1;
 }
 
+function getRandomMove() {
+  let number= randomNumber();
+  while(madeMoves.includes(number)){
+    number=randomNumber();
+  }
+  
+  madeMoves.push(number);
+  return number;
+}
+function randomNumber(){
+  return Math.trunc(Math.random() * 9) + '' + Math.trunc(Math.random() * 9);
+  
+}
 export default playEnemyRound;

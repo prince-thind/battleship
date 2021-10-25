25b803880241861eb0faa65c075859e31c062bdc
Prince-Thind <princethind799@gmail.com>
2021-10-25 12:21:40 +0530

    improve AI design

--- src/components/modules/main/playEnemyRound.js
+++ src/components/modules/main/playEnemyRound.js
@@ -10,10 +10,8 @@ function playEnemyRound() {
   } else {
     const lastElement = movesToMake.pop();
     makeMove(lastElement);
-    
   }
-  // console.log(movesToMake);
-  
+  console.log('movestomake', movesToMake);
 }
 
 function makeMove(number) {
@@ -27,36 +25,44 @@ function makeMove(number) {
 
 function createMoves(number) {
   const res = [];
-  const nineElements = getNineNumbers(number);
-  // console.log(nineElements);
-  for (let number of nineElements) {
+  const adjacentNumbers = getAdjacentNumbers(number);
+  for (let number of adjacentNumbers) {
     if (!number) break;
-    console.log({number,nineElements});
     const gridElement = battleGround.player[number[0]][number[1]];
     if (!gridElement.hit) {
       res.push(number[0] + '' + number[1]);
     }
   }
-  movesToMake = res;
+  console.log('newmoves', res);
+  appendUnique(movesToMake, res);
 }
 
-function getNineNumbers(number) {
+function appendUnique(target, source) {
+  for (const number of source) {
+    if (!target.includes(number)) {
+      target.push(number);
+    }
+  }
+}
+
+function getAdjacentNumbers(number) {
   const number1 = number[0];
   const number2 = number[1];
   const res = Array(9).fill(null);
   for (let i = -1, k = 0; i <= 1; i++) {
     for (let j = -1; j <= 1; j++) {
-      const targetNumber = (+number1 + i) + '' + (+number2 + j);
+      if (i == j && i == 0) continue;
+      const targetNumber = +number1 + i + '' + (+number2 + j);
       if (numberValid(targetNumber)) {
         res[k++] = targetNumber;
       }
-      // console.log(res);
     }
   }
+  console.log('9nums', res);
   return res;
 
   function numberValid(num) {
-    if (num[0] < 0 || num[1] < 0) return false;
+    if (num[0] == '-' || num[2] == '-' || num[1] == '-') return false;
     if (num[0] > 8 || num[1] > 8) return false;
     return true;
   }
